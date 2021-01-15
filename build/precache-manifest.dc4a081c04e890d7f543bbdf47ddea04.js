0f0d48f0bd6779f60039d54e4991d6e49b446fc8
Alhusseinr <Alhusseinr1@montclair.edu>
2021-01-15 16:57:00 -0500

    pushing build

---
+++ build/precache-manifest.dc4a081c04e890d7f543bbdf47ddea04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183f7ace6602ad98bd5bf9d239cd5707",
+    "url": "/ramialhussein/index.html"
+  },
+  {
+    "revision": "1464015714af6d5a7600",
+    "url": "/ramialhussein/static/css/main.8e879629.chunk.css"
+  },
+  {
+    "revision": "9b26d4a423722360250f",
+    "url": "/ramialhussein/static/js/2.0a9aba1a.chunk.js"
+  },
+  {
+    "revision": "fdb2bbbbb647ba518ef54493c5cba538",
+    "url": "/ramialhussein/static/js/2.0a9aba1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1464015714af6d5a7600",
+    "url": "/ramialhussein/static/js/main.695b3434.chunk.js"
+  },
+  {
+    "revision": "2a364bce96498bf45a4f",
+    "url": "/ramialhussein/static/js/runtime-main.f76761b6.js"
+  },
+  {
+    "revision": "fb77ce93a6211f2712b5f9b114a9d5ff",
+    "url": "/ramialhussein/static/media/jumbo2.fb77ce93.jpg"
+  },
+  {
+    "revision": "476e815da4aaf5f2bc1cd88e0e6e6302",
+    "url": "/ramialhussein/static/media/parallex.476e815d.jpg"
+  },
+  {
+    "revision": "0624606fd45f1eb7973993eb2c5cc4d1",
+    "url": "/ramialhussein/static/media/resume.0624606f.png"
+  }
+]);
